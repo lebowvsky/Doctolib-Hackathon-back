8e121afebfc7fcd9ad6b2bb0bccbb20044e443a8
Tobii777 <robin.dorion@gmail.com>
2020-06-25 12:25:00 +0200

    Adding routes to index

--- index.js
+++ index.js
@@ -10,42 +10,7 @@ app.use(express.urlencoded({
   extended: true
 }));
 
-
-// PATIENT
-app.post('/api/patients', (req, res) => {
-  const formData = req.body;
-  connection.query('INSERT INTO patient SET ?', formData, (err, results) => {
-    if (err) {
-      console.log(err);
-      res.status(500).send(err.message);
-    } else {
-      res.sendStatus(200);
-    }
-  });
-});
-
-app.get('/api/patients', (req, res) => {
-  connection.query('SELECT * FROM patient', (err, results) => {
-    if (err) {
-      res.status(500).send(err.message);
-    } else {
-      res.json(results);
-    }
-  });
-});
-
-app.get('/api/patients/:id', (req, res) => {
-  const idPatient = req.params.id;
-  connection.query('SELECT * FROM patient WHERE id WHERE id = ?', idPatient, (err, results) => {
-    if (err) {
-      res.status(500).send(err.message);
-    } else {
-      res.json(results);
-    }
-  })
-})
-
-//MEDECIN
+//GET ALL MEDECINS
 app.get('/api/medecins', (req, res) => {
   connection.query('SELECT * FROM medecin', (err, results) => {
     if (err) {
@@ -56,6 +21,7 @@ app.get('/api/medecins', (req, res) => {
   });
 });
 
+//POST NEW MEDECIN
 app.post('/api/medecins', (req, res) => {
   const formData = req.body;
   connection.query('INSERT INTO medecin SET ?', formData, (err, results) => {
@@ -68,9 +34,10 @@ app.post('/api/medecins', (req, res) => {
   });
 });
 
-//COMMANDE
-app.get('/api/commandes', (req, res) => {
-  connection.query('SELECT * FROM commande', (err, results) => {
+//GET MEDECIN FROM ID
+app.get('/api/medecins/:id', (req, res) => {
+  const idMedecin = req.params.id;
+  connection.query('SELECT * FROM medecin WHERE id WHERE id = ?', idMedecin, (err, results) => {
     if (err) {
       res.status(500).send(err.message);
     } else {
@@ -79,9 +46,21 @@ app.get('/api/commandes', (req, res) => {
   });
 });
 
-app.post('/api/commandes', (req, res) => {
+//GET ALL PATIENTS
+app.get('/api/patients', (req, res) => {
+  connection.query('SELECT * FROM patient', (err, results) => {
+    if (err) {
+      res.status(500).send(err.message);
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+//POST NEW PATIENT
+app.post('/api/patients', (req, res) => {
   const formData = req.body;
-  connection.query('INSERT INTO commande SET ?', formData, (err, results) => {
+  connection.query('INSERT INTO patient SET ?', formData, (err, results) => {
     if (err) {
       console.log(err);
       res.status(500).send(err.message);
@@ -91,7 +70,31 @@ app.post('/api/commandes', (req, res) => {
   });
 });
 
-//ORDONNANCE
+//GET PATIENT FROM ID
+app.get('/api/patients/:id', (req, res) => {
+  const idPatient = req.params.id;
+  connection.query('SELECT * FROM patient WHERE id WHERE id = ?', idPatient, (err, results) => {
+    if (err) {
+      res.status(500).send(err.message);
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+//GET ALL PATIENTS FROM ID MEDECIN
+app.get(`/api/medecins/:id/patients`, (req, res) => {
+  const idMedecin = req.params.id;
+  connection.query('SELECT p.nom, p.prenom, m.id, o.id FROM medecin AS m JOIN ordonnance AS o ON o.id_medecin=m.id JOIN patient AS p ON o.id_patient=p.id WHERE m.id = ?', idMedecin, (err, results) => {
+    if (err) {
+      res.status(500).send(err.message);
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+//GET ALL ORDONNANCES ???
 app.get('/api/ordonnances', (req, res) => {
   connection.query('SELECT * FROM ordonnance', (err, results) => {
     if (err) {
@@ -102,9 +105,58 @@ app.get('/api/ordonnances', (req, res) => {
   });
 });
 
+//GET ALL ORDONNANCES FROM ID PATIENT
+app.get(`/api/patients/:id/ordonnances`, (req, res) => {
+  const idPatient = req.params.id;
+  connection.query('SELECT o.id, m.nom, m.prenom FROM medecin AS m JOIN ordonnance AS o ON m.id=o.id_medecin WHERE id_patient = ?', idPatient, (err, results) => {
+    if (err) {
+      res.status(500).send(err.message);
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+//GET ALL ORDONNANCES FROM ID MEDECIN
+app.get(`/api/medecins/:id/ordonnances`, (req, res) => {
+  const idMedecin = req.params.id;
+  connection.query('SELECT o.id, p.nom, p.prenom FROM medecin AS m JOIN ordonnance AS o ON m.id=o.id_medecin JOIN patient AS p ON o.id_patient=p.id WHERE id_medecin = ?', idMedecin, (err, results) => {
+    if (err) {
+      res.status(500).send(err.message);
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+//POST NEW ORDONNANCE
 app.post('/api/ordonnances', (req, res) => {
   const formData = req.body;
   connection.query('INSERT INTO ordonnance SET ?', formData, (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err.message);
+    } else {
+      res.sendStatus(200);
+    }
+  });
+});
+
+//GET ALL COMMANDES ???
+app.get('/api/commandes', (req, res) => {
+  connection.query('SELECT * FROM commande', (err, results) => {
+    if (err) {
+      res.status(500).send(err.message);
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+//POST NEW COMMANDE
+app.post('/api/commandes', (req, res) => {
+  const formData = req.body;
+  connection.query('INSERT INTO commande SET ?', formData, (err, results) => {
     if (err) {
       console.log(err);
       res.status(500).send(err.message);
