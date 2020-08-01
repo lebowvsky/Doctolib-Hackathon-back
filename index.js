69a7dc8fdb9e182237d3624bcb5322bf96c00da8
Brice Le Gallo <lebowvsky@gmail.com>
2020-08-01 23:37:30 +0200

    port on environment variable

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const connection = require("./conf");
 const app = express();
 const cors = require("cors");
-const port = 8080;
+const port = process.env.PORT || 80;
 
 app.use(cors());
 
